1bdf9b52b31f1c74a758f03513dbcd8ba35b36a9
jpedrocavalo <139908442+jpedrocavalo@users.noreply.github.com>
2026-03-23 21:27:14 -0300

    Add files via upload

---
+++ nomes-config.js
@@ -0,0 +1,135 @@
+window.PEOPLE_CONFIG = {
+  "Lucas V": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Pedro L": {
+    categories: ["c1", "c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Murilo": {
+    categories: ["c1", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: 2
+    }
+  },
+  "Ellen": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Victor": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Jp Souza": {
+    categories: ["c1", "c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Ana Clara": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: 2
+    }
+  },
+  "Miguelito": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Maria L": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: 2
+    }
+  },
+  "Lucas J": {
+    categories: ["c1", "c2"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Edu": {
+    categories: ["c1",],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Joao P": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Andrey": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Sam B": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Sofia": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Paula": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Anna": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Leo": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  },
+  "Vini": {
+    categories: ["c2", "pc"],
+    rules: {
+      noConsecutiveWeeks: true,
+      maxTotal: null
+    }
+  }
+};
